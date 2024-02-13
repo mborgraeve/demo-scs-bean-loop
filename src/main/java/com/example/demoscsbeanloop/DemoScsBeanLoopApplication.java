70b34434289b63da4a6d458c35a6e0bf3d3ff65d
Matthieu Borgraeve <3082664+mborgraeve@users.noreply.github.com>
2024-02-12 19:59:53 -0500

    init

---
+++ src/main/java/com/example/demoscsbeanloop/DemoScsBeanLoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoscsbeanloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoScsBeanLoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoScsBeanLoopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoscsbeanloop/DemoScsBeanLoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoscsbeanloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoScsBeanLoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
